e08121201834a574e1b5542ff5035d9d82ee16d2
Lukasz Lenard <deckardless@gmail.com>
2017-11-08 11:57:54 +0100

    Init commit

---
+++ src/Start.java
@@ -0,0 +1,61 @@
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int myPcRam = 16;
+        int myLaptopRam = 8;
+
+        System.out.println(myLaptopRam + myPcRam);
+        System.out.println(myPcRam % myLaptopRam);
+
+        myLaptopRam = myLaptopRam + 8;
+        System.out.println(myLaptopRam);
+
+        myPcRam = myPcRam - myLaptopRam;
+
+        //Skrócony zapis:
+
+        myPcRam -= myLaptopRam;
+        System.out.println(myPcRam);
+
+        //inkrementacja:
+
+        int counter = 0;
+        counter = counter + 1;
+        counter += 1;
+        counter++;
+
+        int someSum = (5 + 5) * 5;
+        System.out.println(someSum);
+
+        int mySalary = 2500;
+        int dayOfWeek = 5;
+        if (mySalary > 2500 && dayOfWeek!=5) {
+            System.out.println("X");
+        }else if(mySalary > 500 && mySalary < 1000) {
+
+        }else{
+            System.out.println("Z");
+        }
+
+        int age = 18;
+        if (age >= 18) {
+            System.out.println("Jesteś osobą pełnoletnią.");
+            } else {
+                System.out.println("Nie jesteś osobą pełnoletnią.");
+            }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wpisz imię");
+        String name = scanner.nextLine();
+        System.out.println("Wpisz wiek");
+        int ageNew = scanner.nextInt();
+        System.out.println(name+ageNew);
+
+
+
+        }
+    }
