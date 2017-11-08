21be9bd4a1ea237ce04c9ebfa6bfa795ea098e56
Oskar Polak <oskarpolak@vp.pl>
2017-11-08 13:59:33 +0100

    asd

--- src/Start.java
+++ src/Start.java
@@ -15,7 +15,7 @@ public class Start {
 
 
         System.out.println("oskar");
-        System.out.println("Moja zmiana 3");
+        System.out.println("a tu bylem ja - oskar");
         System.out.println("cos");
 
         System.out.println("lukasz");
